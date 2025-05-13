f71b6ce932076940e9136cc5ea94e1da07b2aaf5
Shivam Jakkanwar <117260868+shiva0123m@users.noreply.github.com>
2025-05-13 21:00:26 +0530

    Time: 80 ms (30.55%) | Memory: 54 MB (7.93%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,14 +4,19 @@ public:
     {
         int n = nums.size(); 
         int cnt = 0; 
+        int prefixsum=0;
+        int remove=0;
+        map<int,int>mp;
+        mp[0]=1;
+        for (int i = 0; i < n; i++) 
+        {
+            prefixsum+=nums[i];
 
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) { 
-                sum += nums[j];
-                if (sum == k)
-                    cnt++;
-            }
+            remove=prefixsum-k;
+            cnt+=mp[remove];
+
+            mp[prefixsum]++;
+
         }
 
         return cnt;
