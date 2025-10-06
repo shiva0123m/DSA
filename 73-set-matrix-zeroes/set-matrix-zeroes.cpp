9e8a598bd065b5909302d7c6daa130c5b872ebf6
Shivam Jakkanwar <117260868+shiva0123m@users.noreply.github.com>
2025-10-06 18:52:17 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (78.50%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,43 +4,39 @@ public:
     {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int> row(n, 0);
-        vector<int> col(m, 0);
+        int col0 = 1;
 
-        // First pass: mark rows and columns that should be zero
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                if(matrix[i][j] == 0)
-                {
-                    row[i] = 1;
-                    col[j] = 1;
+        // First pass: mark the rows and columns
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) col0 = 0; // mark if first column needs to be zero
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0; // mark the row
+                    matrix[0][j] = 0; // mark the column
                 }
             }
         }
 
-        // Second pass: set marked rows to zero
-        for(int i = 0; i < n; i++)
-        {
-            if(row[i] == 1)
-            {
-                for(int j = 0; j < m; j++)
-                {
+        // Second pass: set elements to 0 based on markers (excluding first row and col)
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        // Third pass: set marked columns to zero
-        for(int j = 0; j < m; j++)
-        {
-            if(col[j] == 1)
-            {
-                for(int i = 0; i < n; i++)
-                {
-                    matrix[i][j] = 0;
-                }
+        // Third pass: handle first row
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Fourth pass: handle first column
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
