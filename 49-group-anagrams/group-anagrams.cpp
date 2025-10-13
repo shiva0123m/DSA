d0d365d2437edb241a2184bfa917f359aa41821c
Shivam Jakkanwar <117260868+shiva0123m@users.noreply.github.com>
2025-10-13 16:00:32 +0530

    Time: 27 ms (25.73%) | Memory: 25.2 MB (61.38%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) 
+    {
+      map<string,vector<string>>ans;
+
+      for(string s : strs)
+      {
+        string key=s;
+        sort(key.begin(),key.end());
+        ans[key].push_back(s);
+      }
+       vector<vector<string>>result;
+      for(auto& entry : ans)
+      {
+            result.push_back(entry.second);
+      }
+      return result;
+
+    }
+};
