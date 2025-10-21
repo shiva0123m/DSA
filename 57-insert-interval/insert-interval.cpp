0d8c106eaf014e08877318b8586cbe7eae316cd5
Shivam Jakkanwar <117260868+shiva0123m@users.noreply.github.com>
2025-10-21 07:31:00 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (94.16%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> result;
-        int i = 0;
-        int n = intervals.size();
-        
-        
-        while (i < n && intervals[i][1] < newInterval[0]) {
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) 
+    {
+        vector<vector<int>>result;
+        int n=intervals.size();
+        int i=0;
+        // Here we are skipinging  all the intervals[i][1] less than newInterval[0]
+        while(i<n && intervals[i][1] < newInterval[0])
+        {
             result.push_back(intervals[i]);
             i++;
         }
-        
-        while (i < n && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
+        // Now we intialize comparing least for newIntervals[0] and Max for newIntervals[1]
+        while(i<n && intervals[i][0]<=newInterval[1])
+        {
+            newInterval[0]=min(intervals[i][0],newInterval[0]);
+            newInterval[1]=max(intervals[i][1],newInterval[1]);
             i++;
-        }
-        result.push_back(newInterval); 
-        
-        while (i < n) {
+        } 
+        result.push_back(newInterval);
+        while(i<n)
+        {
             result.push_back(intervals[i]);
             i++;
         }
-
-        return result;
+        return result;   
     }
 };
