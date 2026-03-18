79fc4ed80c0a6641abd2f933b2cb6b6975a7a895
Shivam Jakkanwar <117260868+shiva0123m@users.noreply.github.com>
2026-03-18 12:30:18 +0530

    Time: 3 ms (45.54%) | Memory: 185.2 MB (39.33%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -1,34 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) 
     {
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode* front=head;
+        ListNode* back=head;
 
-        if(head==NULL)return head;
-        
-        ListNode* first = head;
-        ListNode* second = head;
-        ListNode* temp = head;
-
-        // move first to kth node
-        for(int i = 1; i < k; i++)
-            first = first->next;
-
-        ListNode* kthFromStart = first;
-
-        // move temp to end while moving second
-        while(first->next != NULL)
+        for(int i=1;i<k;i++)
         {
-            first = first->next;
-            second = second->next;
+            front=front->next;
         }
+        ListNode* kthFromBegin=front;
+        ListNode* temp=dummy;
+        int count=0;
+        while(temp!=NULL)
+        {
+            temp=temp->next;
+            count++;
+        }
+        int fromLast=count-k;
+        temp=dummy;
+        for(int i=0;i<fromLast;i++)
+        {
+            temp=temp->next;
+        }
+        ListNode* kthFromLast=temp;
 
-        ListNode* kthFromEnd = second;
+        int value=kthFromBegin->val;
+        kthFromBegin->val=kthFromLast->val;
+        kthFromLast->val=value;
 
-        // swap values
-        int val = kthFromStart->val;
-        kthFromStart->val = kthFromEnd->val;
-        kthFromEnd->val = val;
-
-        return head;
+        return dummy->next;
     }
 };
