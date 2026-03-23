969c0c0f15d84469c33dd83c3bd54ca91e8305cd
Shivam Jakkanwar <117260868+shiva0123m@users.noreply.github.com>
2026-03-23 12:46:26 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (38.65%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -1,32 +1,44 @@
-#include <unordered_set>
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        if (!head) return nullptr;  
-
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-
-        unordered_set<int> duplicates;
-        
-        ListNode* curr = head;
-        while (curr && curr->next) {
-            if (curr->val == curr->next->val) {
-                duplicates.insert(curr->val);
-            }
-            curr = curr->next;
+    ListNode* deleteDuplicates(ListNode* head) 
+    {
+        if(head==NULL)
+        {
+            return head;
         }
-        
-        curr = dummy;
-        while (curr && curr->next) {
-            if (duplicates.count(curr->next->val)){
-                curr->next = curr->next->next;
-            } else {
-                curr = curr->next;
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+
+        ListNode* curr=head;
+        ListNode* temp=dummy;
+
+        while(curr!=NULL)
+        {
+            if(curr->next!=NULL && curr->next->val==curr->val)
+            {
+                int value=curr->val;
+                while(curr!=NULL && curr->val==value)
+                {
+                    curr=curr->next;
+                }
+                temp->next=curr;
             }
-        }
-        
-        return dummy->next; 
+            else
+            {
+                temp=curr;
+                curr=curr->next;
+            }
+        }  
+        return dummy->next;                   
     }
 };
