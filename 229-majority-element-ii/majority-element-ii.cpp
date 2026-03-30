4fa42253a311fc75b0f62e4ec6bd172fda9f6642
Shivam Jakkanwar <117260868+shiva0123m@users.noreply.github.com>
2026-03-30 12:58:41 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (83.06%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -4,21 +4,61 @@ public:
     {
         int n=nums.size();
 
-        map<int,int>mp;
-        int max=0;
-        vector<int>ans;
-        for(int i=0;i<n;i++)
+        // Handling Base case
+        if(n==0)
         {
-           mp[nums[i]]++;
+            return nums;
         }    
 
-        for(auto it :mp)
+        int candidate=0;
+        int candidate1=1;
+        int count=0;
+        int count1=0;
+
+        // Calculating candidate occurrence and ,Moore's Voting alogirthm implimentaion
+        for(int i=0;i<n;i++)
         {
-            if(it.second>n/3)
+            if(nums[i]==candidate)
             {
-                ans.push_back(it.first);
+                count++;
+            }
+            else if(nums[i]==candidate1)
+            {
+                count1++;
+            }
+            else if(count==0)
+            {
+                candidate=nums[i];
+                count=1;
+            }
+            else if(count1==0)
+            {
+                candidate1=nums[i];
+                count1++;
+            }
+            else
+            {
+                count--;
+                count1--;
             }
         }
+
+       count=0;
+       count1=0;
+
+        
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==candidate)count++;
+            if(nums[i]==candidate1)count1++;
+        }
+
+        vector<int>ans;
+
+        // Cross Verification
+        if(count>n/3)ans.push_back(candidate);
+        if(count1>n/3)ans.push_back(candidate1);
+
         return ans;
     }
 };
