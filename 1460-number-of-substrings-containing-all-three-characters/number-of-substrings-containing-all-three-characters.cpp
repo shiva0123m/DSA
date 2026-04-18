883dacf1683320b314e2cf4377f78972cecd4f30
Shivam Jakkanwar <117260868+shiva0123m@users.noreply.github.com>
2026-04-18 18:33:00 +0530

    Time: 26 ms (22.40%) | Memory: 11.2 MB (19.95%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,19 +1,23 @@
-class Solution {
-public:
-    int numberOfSubstrings(string s) {
-        int count[3] = {0};  // count of 'a', 'b', 'c'
-        int res = 0, left = 0;
-        
-        for (int right = 0; right < s.length(); ++right) {
-            count[s[right] - 'a']++;
+class Solution 
+{
+    public:
+        int numberOfSubstrings(string s) 
+        {
+            map<char,int> mp;
+            int left = 0;
+            int ans = 0;
+            int n=s.length();
 
-            // shrink window from left until it still contains all 3
-            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
-                res += s.length() - right;  // all substrings starting from `left` to `right` are valid
-                count[s[left] - 'a']--;
-                left++;
+            for (int right = 0; right < s.size(); right++) {
+                mp[s[right]]++;
+
+                while (mp['a'] > 0 && mp['b'] > 0 && mp['c'] > 0) 
+                {
+                    ans += (n - right);
+                    mp[s[left]]--;
+                    left++; 
+                }
             }
+            return ans;
         }
-        return res;
-    }
 };
