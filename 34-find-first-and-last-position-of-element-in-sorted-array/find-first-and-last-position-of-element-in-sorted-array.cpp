f3935b686579c0e1f368992836013c62999bc980
Shivam Jakkanwar <117260868+shiva0123m@users.noreply.github.com>
2026-05-27 16:43:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (88.10%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,41 +1,16 @@
 class Solution {
 public:
-    int findFirst(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1, result = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                result = mid;
-                high = mid - 1;  // continue searching on the left side
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return result;
-    }
+    vector<int> searchRange(vector<int>& nums, int target) 
+    {
+        int left = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
 
-    int findLast(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1, result = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                result = mid;
-                low = mid + 1;  // continue searching on the right side
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+        if (left == nums.size() || nums[left] != target)
+        {
+            return {-1, -1};
         }
-        return result;
-    }
 
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans(2, -1);
-        ans[0] = findFirst(nums, target);
-        ans[1] = findLast(nums, target);
-        return ans;
+        int right = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+
+        return {left, right - 1};
     }
 };
