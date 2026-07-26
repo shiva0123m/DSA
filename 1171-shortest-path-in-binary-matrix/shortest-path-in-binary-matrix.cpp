8617673fdcc8f6c8b0e84c3c2cfe4649647d8bc5
Shivam Jakkanwar <117260868+shiva0123m@users.noreply.github.com>
2026-07-26 14:24:48 +0530

    Time: 9 ms (92.56%) | Memory: 24.5 MB (71.74%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid)
-    {
-        
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+
         int n = grid.size();
-        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0)
+
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1)
             return -1;
 
-        // 8 directions (including diagonals)
-        vector<pair<int, int>> directions = {
-            {-1, -1}, {-1, 0}, {-1, 1},
-            {0, -1},          {0, 1},
-            {1, -1}, {1, 0}, {1, 1}
-        };
+        queue<pair<pair<int,int>,int>> q;
+        q.push({{0,0},1});
 
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-        grid[0][0] = 1;  
+        grid[0][0] = 1;
 
-        while (!q.empty()) {
-            auto [r, c] = q.front();
+        int dr[] = {-1,-1,-1,0,0,1,1,1};
+        int dc[] = {-1,0,1,-1,1,-1,0,1};
+
+        while(!q.empty())
+        {
+            auto cur = q.front();
             q.pop();
-            int dist = grid[r][c];
 
-            if (r == n - 1 && c == n - 1)
+            int r = cur.first.first;
+            int c = cur.first.second;
+            int dist = cur.second;
+
+            if(r == n-1 && c == n-1)
                 return dist;
 
-            for (auto [dr, dc] : directions) {
-                int nr = r + dr, nc = c + dc;
-                if (nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nr][nc] == 0) {
-                    grid[nr][nc] = dist + 1;
-                    q.push({nr, nc});
+            for(int i=0;i<8;i++)
+            {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                if(nr>=0 && nc>=0 && nr<n && nc<n && grid[nr][nc]==0)
+                {
+                    grid[nr][nc] = 1;
+                    q.push({{nr,nc},dist+1});
                 }
             }
         }
