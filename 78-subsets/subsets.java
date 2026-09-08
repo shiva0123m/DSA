d8d436ec667e0e8aa683a6bd8083b7f6cede61cb
Shivam Jakkanwar <117260868+shiva0123m@users.noreply.github.com>
2026-09-08 13:25:46 +0530

    Time: 2 ms (4.12%) | Memory: 44.5 MB (6.78%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void generateSubsets(int i,int n,List<List<Integer>> ans,List<Integer> subsets,int[] nums)
+    {
+        if(i==n)
+        {
+            ans.add(new ArrayList(subsets));
+            return;
+        }
+        subsets.add(nums[i]);
+        generateSubsets(i+1,n,ans,subsets,nums);
+        subsets.remove(subsets.size() - 1);
+        generateSubsets(i+1,n,ans,subsets,nums);
+    }
+    public List<List<Integer>> subsets(int[] nums) 
+    {
+        Arrays.sort(nums);
+        List<List<Integer>> ans= new ArrayList<>();    
+        List<Integer> subsets=new ArrayList<>();
+
+        generateSubsets(0,nums.length,ans,subsets,nums);
+        return ans;
+    }
+}
