95d350c0f5af7f9cdd1bb197d2529a82bdff565f
Shivam Jakkanwar <117260868+shiva0123m@users.noreply.github.com>
2026-09-12 17:09:02 +0530

    Time: 6 ms (17.49%) | Memory: 47.2 MB (9.32%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,38 +1,36 @@
-class Solution 
-{
-    private void swap(int[] nums, int i, int j) 
+class Solution {
+    public void swap(int[] nums,int i,int index)
     {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        int temp=nums[i];
+        nums[i]=nums[index];
+        nums[index]=temp;
     }
-    public void findAllPermutations(int index,List<List<Integer>> ans,int[] nums)
+    public void findTotalSubsets(int index,int[]  nums,int n,List<List<Integer>>ans)
     {
-         if(index==nums.length)
+        if(index==n)
         {
-            ans.add(
-                Arrays.stream(nums)
-                    .boxed()
-                    .collect(Collectors.toList())
-            );
+            ans.add(Arrays.stream(nums)
+                                   .boxed()
+                                   .collect(Collectors.toList()));
             return;
         }
-
-        Set<Integer> visited= new HashSet<>();
+        Set<Integer> visited = new HashSet<>();
         for(int i=index;i<nums.length;i++)
         {
             if(visited.contains(nums[i]))continue;
 
             visited.add(nums[i]);
             swap(nums,i,index);
-            findAllPermutations(index+1,ans,nums);
+            findTotalSubsets(index+1,nums,nums.length,ans);
             swap(nums,i,index);
         }
+        
     }
-    public List<List<Integer>> permuteUnique(int[] nums) 
-    {
-        List<List<Integer>>ans=new ArrayList<>();
-        findAllPermutations(0,ans,nums);
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> subset= new ArrayList<>();
+
+        findTotalSubsets(0,nums,nums.length,ans);
         return ans;
     }
 }
